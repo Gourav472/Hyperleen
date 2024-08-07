c49ba30217b764f3d5c5ffe95410ab6c87e059b5
akshaybishnoi01 <akshaybishnoi0008@gmail.com>
2024-08-07 14:37:01 +0530

    update commn btn

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/Header';
+import Verification from './components/Verification';
 
 function App() {
   return (
     <div>
       <Header />
+      <Verification />
     </div>
   );
 }
