7aa06c7abe9f6a4a54743f68b911d44bbda60eac
Gourav472 <149783900+Gourav472@users.noreply.github.com>
2024-08-09 14:06:39 +0530

    meta tags added

--- tailwind.config.js
+++ tailwind.config.js
@@ -56,16 +56,12 @@ module.exports = {
       boxShadow: {
         '2xl': "4px 4px 20px 0px #084CAF4D",
         '3xl': "0px 4px 16px 0px #084CAF1F",
+        '4xl' : "4.11px -4.29px 112.6px -10px #084CAF14",
+        '5xl' : "0px 4px 4px 0px #00000040",
         '6xl': "-5px 0px 42.77px -15.24px #6F69F280",
         '7xl': "0.88px -8.24px 35.83px 0px #6F69F21F",
         '8xl': "0px 0px 42.77px -10.24px #6F69F280",
-        '3xl_white': "0px 0px 15px white",
-        
-      },
-      boxShadow: {
-        '2xl' : "4px 4px 20px 0px #084CAF4D",
-        '4xl' : "4.11px -4.29px 112.6px -10px #084CAF14",
-        '5xl' : "0px 4px 4px 0px #00000040",
+        '9xl': "0px 0px 15px white",
       },
       backgroundImage: {
         'gradientBorder':"linear-gradient(90deg, rgba(199, 199, 199, 0) 0%, #CDCDCD 34.5%, #CDCDCD 65%, rgba(196, 196, 196, 0) 100%)",
