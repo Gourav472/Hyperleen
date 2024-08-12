f329c29882939915904e0c97d3ee01c4f0604086
Gourav472 <149783900+Gourav472@users.noreply.github.com>
2024-08-12 11:17:51 +0530

    commonbtn duration increased

--- src/common/CommonBtn.jsx
+++ src/common/CommonBtn.jsx
@@ -6,7 +6,7 @@ const CommonBtn = ({ className, text, onClick }) => {
       onClick={onClick} 
       className={`${className} max-md:py-3 md:py-[14px] border border-darkBlue shadow-2xl hover:shadow-none hover:border-black text-center relative rounded-[100px] group overflow-hidden bg-darkBlue text-white inline-block`}
     >
-      <span className="absolute top-0 left-0 flex w-full h-0 mb-0 ease-linear duration-200 transform translate-y-0 bg-white group-hover:h-full "></span>
+      <span className="absolute top-0 left-0 flex w-full h-0 mb-0 ease-linear duration-300 transform translate-y-0 bg-white group-hover:h-full "></span>
       <span className="relative group-hover:text-black font-poppins !leading-[26px] max-md:text-base md:text-lg font-normal">
         {text}
       </span>
